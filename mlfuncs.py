984ccf69be21c936e87f0c49fab5199ce73647cf
Leonard Paul Kamara <lenpk3@yahoo.co.uk>
2021-12-13 02:40:38 -0800

    created function to help in pre ml preprocessing

--- mlfuncs.py
+++ mlfuncs.py
@@ -32,7 +32,28 @@ import catboost
 # set seed for reproducibility
 SEED = 24
 # process data before machine learning preprocessing
-def pre_ml_preprocessing(df):
-    
+def pre_ml_preprocessing(df, initial_to_drop, num_cols, target_var = None, num_cols_threshold = 0.9, low_var_threshold = 0.9):
+    # check for valid dataframe
+    if isinstance(df, pd.DataFrame):
+        # extract dataframe columns
+        df_cols = df.columns.tolist()
+        # check if all columns to drop are in df_cols
+        membership = all(col in df_cols for col in initial_to_drop)
+        # if membership
+        if membership:
+            for col in initial_to_drop:
+                # drop col
+                print("Dropping: {}".format(col))
+                df.drop(col, axis=1, inplace=True)
+        else:
+            not_cols = []
+            for col in initial_to_drop:
+                if col not in df_cols:
+                    not_cols.append(col)
+            raise utils.InvalidColumn(not_cols)
+        # drop high correlated features
+        df = funcs.dim_redux(df, num_cols, threshold = num_cols_threshold)
+        # drop low variance features
+        df = funcs.drop_low_var_cols(df, target_var = target_var, unique_val_threshold = low_var_threshold)
     
     return df
